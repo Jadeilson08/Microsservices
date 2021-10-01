8a85132434f3f0ea8de1668ee5b10c43aeba72b5
Jadeilson Leonardo Trizotti Rodrigues <jadeilson08@gmail.com>
2021-10-01 13:50:19 -0400

    microsservice basket

---
+++ Basket.API/Entities/ShoppingCar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCar
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCarItem> Items { get; set; } = new List<ShoppingCarItem>();
+
+        public ShoppingCar()
+        {
+        }
+
+        public ShoppingCar(string name)
+        {
+            UserName = name;
+        }
+
+        public decimal TotalPrice 
+        {
+            get 
+            {
+                decimal price = 0;
+                foreach (ShoppingCarItem item in Items)
+                {
+                    price += item.Price * item.Quantity;
+                }
+                return price;
+            }
+        }
+    }
+}
