599a104ed70e8052baa23603d7a8dcfdb45ed488
Jadeilson Leonardo Trizotti Rodrigues <jadeilson08@gmail.com>
2021-10-14 07:56:19 -0400

    Discount API

---
+++ Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,39 @@
+﻿using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Coupon> GetDiscount(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
