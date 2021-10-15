618a71381ff959118e054e3ca9098d3956398279
Jadeilson Leonardo Trizotti Rodrigues <jadeilson08@gmail.com>
2021-10-15 07:56:05 -0400

    container discount

---
+++ Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,47 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet("{product}")]
+        public async Task<IActionResult> Get(string product)
+        {
+            return Ok(await repository.GetDiscount(product));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Coupon coupon)
+        {
+            return Ok(await repository.CreateDiscount(coupon));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody]Coupon coupon)
+        {
+            return Ok(await repository.UpdateDiscount(coupon));
+        }
+        
+        [HttpDelete("{product}")]
+        public async Task<IActionResult> Delete(string product)
+        {
+            return Ok(await repository.DeleteDiscount(product));
+        }
+    }
+}
--- Discount.API/Repositories/DiscountRepository.cs
+++ Discount.API/Repositories/DiscountRepository.cs
@@ -1,5 +1,7 @@
-﻿using Discount.API.Entities;
+﻿using Dapper;
+using Discount.API.Entities;
 using Microsoft.Extensions.Configuration;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,24 +18,64 @@ namespace Discount.API.Repositories
             this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
-        public async Task<bool> CreateDiscount(Coupon coupon)
-        {
-            throw new NotImplementedException();
-        }
-
         public async Task<Coupon> GetDiscount(string name)
         {
-            throw new NotImplementedException();
+            var connection = GetConnection();
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                    ("SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = name });
+
+            if (coupon is null)
+                return new Coupon() { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            return coupon;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            var connection = GetConnection();
+
+            var affected = await connection.ExecuteAsync(
+                "INSERT INTO Coupon (ProductName, Description, Amount)" +
+                "VALUES (@ProductName, @Description, @Amount)",
+                new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+
+            return Convert.ToBoolean(affected);
         }
 
         public async Task<bool> UpdateDiscount(Coupon coupon)
         {
-            throw new NotImplementedException();
+            var connection = GetConnection();
+
+            var affected = await connection.ExecuteAsync(
+                "UPDATE Coupon " +
+                "SET ProductName = @ProductName, Description = @Description, Amount = @Amount WHERE Id = @Id",
+                new
+                {
+                    ProductName = coupon.ProductName,
+                    Description = coupon.Description,
+                    Amount = coupon.Amount,
+                    Id = coupon.Id
+                });
+
+            return Convert.ToBoolean(affected);
         }
 
         public async Task<bool> DeleteDiscount(string productName)
         {
-            throw new NotImplementedException();
+            var connection = GetConnection();
+
+            var affected = await connection.ExecuteAsync(
+                "DELETE FROM Coupon " +
+                "WHERE ProductName = @ProductName",
+                new { productName });
+
+            return Convert.ToBoolean(affected);
+        }
+
+        private NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionStrings"));
         }
     }
 }
