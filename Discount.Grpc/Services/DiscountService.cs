5589920c9685f3f5c7a7b47b69995981bc79b89a
Jadeilson Leonardo Trizotti Rodrigues <jadeilson08@gmail.com>
2021-10-19 13:55:41 -0400

    Discount Service

---
+++ Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository repository;
+        private readonly IMapper mapper;
+
+        public DiscountService(IDiscountRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await repository.GetDiscount(request.ProductName);
+            
+            if(coupon is null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount Product {request.ProductName} Not Found"));
+            
+            var couponModel = mapper.Map<CouponModel>(coupon);
+            
+            return couponModel;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = mapper.Map<Coupon>(request.Coupon);
+
+            var isSaved = await repository.CreateDiscount(coupon);
+
+            if (!isSaved)
+                throw new RpcException(new Status(StatusCode.Internal, $"Discount Product {coupon.ProductName} Not Found"));
+
+            return mapper.Map<CouponModel>(coupon);
+        }
+    }
+}
